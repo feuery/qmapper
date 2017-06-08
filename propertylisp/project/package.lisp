aff68186bdee4ef7aede74d1fd61f5f36f64518a
Feuer <ilpo.lehtinen.1@saunalahti.fi>
2017-06-08 19:44:38 +0300

    Generating new cpp sources when there's a write to the lisp definition file mostly works. It just generates invalid c++ :)

--- propertylisp/project/package.lisp
+++ propertylisp/project/package.lisp
@@ -1,5 +1,13 @@
 ;;;; package.lisp
 
 (defpackage #:propertier
-  (:use #:cl #:cl-arrows #:cl-fsnotify #:lparallel.queue #:sb-thread))
+  (:use #:cl #:cl-arrows #:cl-fsnotify #:lparallel.queue #:sb-thread)
+  (:import-from #:sb-posix :getcwd)
+  (:export :defcppclass
+	   :protected
+	   :public
+	   :private
+	   :properties
+	   :functions
+	   :fields))
 
--- propertylisp/project/propertier.lisp
+++ propertylisp/project/propertier.lisp
@@ -1,7 +1,6 @@
 ;;;; propertier.lisp
 
 ;; (asdf:load-system "propertier")
-
 (in-package #:propertier)
 
 ;; (ql:quickload "cl-arrows")
@@ -16,16 +15,31 @@
 ;; 			   :author "feuer <feuer@feuerx.net>")
 
 (defvar property-container '())
+(defvar *compiled-class* nil)
+
+(defun slurp (path)
+  (with-open-file (stream path)
+    (let ((data (make-string (file-length stream))))
+      (read-sequence data stream)
+      data)))
 
 (defun read-file (file)
-  (read-from-string 
-   (format nil "~{~A~^~%~}"
-	   (with-open-file (f file)
-	     (loop for line = (read-line f nil :eof) 
-		until (eq line :eof)
-		collecting line)))))
+  (let ((code (-> file
+		  slurp
+		  read-from-string)))
+    (setf (nth 1 code) `(quote ,(nth 1 code)))
+    (format t "Code: ~A~%" code)
+    code))
 
-;; (open-fsnotify
+(defun str (&rest s)
+  (->> s
+       (mapcar #'(lambda (ss)
+		   (if (symbolp ss)
+		       (symbol-name ss)
+		       ss)))
+		   
+       (cons 'string)
+       (apply #'concatenate)))
 
 (defun last-satisfies (pred coll)
   (-> pred
@@ -60,9 +74,9 @@
 
 (defmacro properties (&rest forms)
   (dolist (l forms)
-    (destructuring-bind (type name default) l
-      ;; (push name propertynames)
-      ;; (push type propertytypes)
+    (let ((type (car l))
+	  (name (cadr l))
+	  (default (caddr l)))
       (push `(,name . ,type) property-container)
       (format t "~A get~A();~%
 void set~A(~A val);~%
@@ -93,16 +107,8 @@ type name default))))
 		       (format nil "~{~A~^, ~}"))))
       (format t "~A ~A(~A);~%" type name params))))
 
-;; (defun get-properties (forms)
-;;    (->> forms
-;; 	(mapcar (lambda (visibility-form)
-;; 		  (->> visibility-form
-;; 		       (remove-if-not (lambda (x)
-;; 					(equal (car x)
-;; 					       'properties
-
 (defun fill-property-names (classname)
-  (format t "~A() {~%" classname)
+  (format t "~A() {~%" (symbol-name classname))
        
   (mapcar (lambda (property-pair i)
 	    (let ((name (car property-pair)))
@@ -135,30 +141,33 @@ type name default))))
 			
 			
 
-(defmacro defcppclass (&rest forms)
-  (let* ((class (car forms)) 
-	 (forms (cdr forms)))
-    ;; (setf propertynames '())
-    ;; (setf propertytypes '())
-    (setf property-container '())
+(defmacro defcppclass (class &rest forms)
+  (setf property-container '())
+  (let ((classname (cadr class)))
     (format t "#include<cstring>~%")
-    (format t "class ~A {~%" class)
+    (format t "class ~A {~%" classname)
     (dolist (form forms)
       (eval form))
     (format t "public: ~%")
-    (fill-property-names class)
-    (format t "};~%")))
+    (fill-property-names classname)
+    (format t "};~%")
+    (setf *compiled-class* class)))
 
 (defun headers (forms)
-  (eval forms))
+  (let ((*print-case* :downcase))
+    (setf *compiled-class* nil)
+    (-> forms
+	eval)))
 
 (defun source (forms)
-  (let ((classname (cadr forms)))
+  (let ((*print-case* :downcase)
+	(classname (symbol-name (cadadr forms))))
+    
     (format t "///////// CPP FILE STARTS HERE ~%")
-    ;; (push `(,name . ,type) property-container)
+    (format t "// classname on ~A~%" classname)
     (dolist (x property-container)
-      (let ((name (car x))
-	    (type (cdr x)))
+      (let ((name (symbol-name (car x)))
+	    (type (symbol-name (cdr x))))
 	(format t "~A ~A::get~A() {~% return ~A_field; ~%};~%"
 		type classname (string-capitalize name) name)
 	(format t "void ~A::set~A(~A val) {~% ~A_field = val; ~%}~%" classname (string-capitalize name) type name)))
@@ -166,14 +175,11 @@ type name default))))
 
 (defvar running? t)
 (defvar *compilation-queue* (make-queue))
-;; (pop-queue *compilation-queue*)
-
-;; (dolist (x (range 20))
-;;   (push-queue (format nil "~A" x) *compilation-queue*))					  
 
 (defun set-watch-on (dirpath)
   (setf running? t)
-  (make-thread (lambda () 
+  (make-thread (lambda ()
+		 (in-package #:propertier)
 		 (open-fsnotify)
 		 (add-watch dirpath)
 		 (loop while running?
@@ -181,41 +187,58 @@ type name default))))
 			 (dolist (event e)
 			   (let ((path (car event))
 				 (type (cdr event)))
-			     ;; Tästä voisi yrittää tehdä jonkun ikuisen striimin?
-			     ;; Josta sitten filtteröidään vain *.def - tiedostot compilation-queueen
 			     (when (or (eq type :CREATE)
 				       (eq type :MODIFY))
-			       (format t "Pushing ~A to compilation queue~%" path)
 			       (push-queue path *compilation-queue*))))
 			 (sleep 3))))))
+
+(defmacro stdout-str (&rest forms)
+  (let ((s (gensym)))
+  `(let* ((,s (make-string-output-stream))
+	  (*standard-output* ,s))
+     (progn
+       ,@forms)
+     (get-output-stream-string ,s))))
 
 (defun start-compilation-server (input-dir output-dir)
   (set-watch-on input-dir)
 
   (make-thread (lambda ()
+		 (in-package #:propertier)
 		 (loop while running?
 		    do (let* ((to-compile (pop-queue *compilation-queue*))
-			      (file-type (-> to-compile
+			      (file-type (-> (format nil "~A" to-compile)					     
 					     reverse
 					     (subseq 0 3)
 					     reverse)))
 			 (if (and (string= file-type "def")
 				  (probe-file to-compile))
-			     (let ((*print-case :downcase))
-			       (format t "~%Compiling ~A~%~%" to-compile))
-			     (format t "~A ei täyttänyt vaatimuksia ~%" to-compile))
+			     (let ((file-contents (read-file to-compile)))
+			       (let ((headd (stdout-str (headers file-contents)))
+				     (cpp (stdout-str (source file-contents))))
+				 (let ((header-filename (str output-dir "/" (symbol-name (cadr *compiled-class*)) ".h"))
+				       (cpp-filename (str output-dir "/" (symbol-name (cadr *compiled-class*)) ".cpp")))
+				   (format t "Outputting sources to ~A and ~A~%" header-filename cpp-filename)
+				   (when *compiled-class*
+				     (with-open-file (f header-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
+				       (write-string headd f))
+				     (with-open-file (f cpp-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
+				       (write-string cpp f))
+				     (setf *compiled-class* nil)
+				     (format t "Wrote files ~a and ~a~%" header-filename cpp-filename))))))
 
 			 (sleep 2))))))
 
-;; (start-compilation-server "/home/feuer/Dropbox/qt-test/propertylisp/project" "")
-
-;; (setf running? nil)
-
-;; (queue-count *compilation-queue*)
-;; (mapcar (lambda (x)
-;; 	  (pop-queue *compilation-queue*)) (range 14))
+(defun reset ()
+  (setf running? nil)
+  (sleep 2)
+  (mapcar (lambda (x)
+	    (pop-queue *compilation-queue*)) (range (queue-count *compilation-queue*)))
+  (start-compilation-server "/home/feuer/Dropbox/qt-test/propertylisp/project" "/home/feuer/Dropbox/qt-test/propertylisp/project"))
 
 ;; (let ((*print-case* :downcase)
 ;;       (forms (read-file "./tile.def")))
 ;;   (headers forms)
 ;;   (source forms))
+(reset)
+(format t "~%Loaded~%")
