4e373f3cf2f5b4f444f81bff43f0121050b5a816
Ilpo Lehtinen <feuer@feuerx.net>
2017-06-23 07:25:52 +0000

    Began work on a compilation work where the compiler reads the state of every *.def file in the *input-directory*, builds up a list of the types of every property in the definitions and outputs a defclass with a special set(const char* name, T value) method for every known type

--- propertylisp/project/propertier.lisp
+++ propertylisp/project/propertier.lisp
@@ -1,5 +1,5 @@
 ;;;; propertier.lisp
-;; (progn (asdf:load-system "propertier") (main))
+;; (asdf:load-system "propertier")
 
 ;; Build by invoking sbcl
 ;; Making sure this file script is loadable by asdf (`ln -s ${pwd} /usr/share/common-lisp/source/propertier` in my arch box)
@@ -9,35 +9,47 @@
 
 (in-package #:propertier)
 
-(defun plistify (l)
-  (assert (= 0 (mod (length l) 2) 0))
-  (defun -plistify (l prop?)
-    (if l
-	(cons (if prop?
-		  (read-from-string (car l))
-		  (car l))
-	      (-plistify (cdr l)
-			 (not prop?)))
-	'()))
-  (-plistify l t))
-
-;; (ql:quickload "cl-arrows")
-;; (ql:quickload "quickproject")
-;; (ql:quickload "cl-fsnotify")
-;; (ql:quickload :lparallel)
-;; (ql:quickload :simple-date-time)
-;; (ql:quickload :cl-ppcre)
-;; (require :quickproject)
-
-;; (quickproject:make-project "~/Dropbox/qt-test/propertylisp/project/"
-;; 			   :depends-on '(cl-arrows)
-;; 			   :name "propertier"
-;; 			   :author "feuer <feuer@feuerx.net>")
-
 (defvar *input-dir* '())
 (defvar *output-dir* '())
-(defvar property-container '())
-(defvar *compiled-class* nil)
+(setf *input-dir* "/home/feuer/qmapper/propertylisp/project")
+(setf *output-dir* "/home/feuer/cpp-uloste")
+
+(defclass propertyclass ()
+  ((classname
+    :initform 'undefined
+    :accessor classname)
+   (propertylist ;; name - type alist
+    :initform '()
+    :accessor propertylist)
+   (header-str
+    :initform ""
+    :accessor header-str)
+   (src-str
+    :initform ""
+    :accessor src-str)
+   (header-filename
+    :initform ""
+    :accessor header-filename)
+   (src-filename
+    :initform ""
+    :accessor src-filename)))
+
+(defun str (&rest s)
+  (->> s
+       (mapcar #'(lambda (ss)
+		   (if (symbolp ss)
+		       (symbol-name ss)
+		       ss)))   
+       (cons 'string)
+       (apply #'concatenate)))
+
+(defmacro stdout-str (&rest forms)
+  (let ((s (gensym)))
+  `(let* ((,s (make-string-output-stream))
+	  (*standard-output* ,s))
+     (progn
+       ,@forms)
+     (get-output-stream-string ,s))))
 
 (defun slurp (path)
   (with-open-file (stream path)
@@ -53,25 +65,7 @@
     (format t "Code: ~A~%" code)
     code))
 
-(defun str (&rest s)
-  (->> s
-       (mapcar #'(lambda (ss)
-		   (if (symbolp ss)
-		       (symbol-name ss)
-		       ss)))   
-       (cons 'string)
-       (apply #'concatenate)))
-
-(defun last-satisfies (pred coll)
-  (-> pred
-      (remove-if-not coll)
-      last
-      car))
-
-(defun range (start &optional end)
-  (if end
-      (loop for i from start below end collect i)
-      (loop for i from 0 below start collect i)))
+(defparameter classobj nil)
 
 (defmacro public (&rest forms)
   (format t "public: ~%")
@@ -114,7 +108,7 @@
 					 butlast
 					 butlast
 					 (format nil "~{~a~^ ~}")) "::")))
-      (push `(,name . ,type) property-container)
+      (push `(,name . ,type) (propertylist classobj))
       (format t "~A get~A();~%
 void set~A(~A val);~%
 
@@ -125,11 +119,11 @@ type name (prin1-to-string default)))))
 (defmacro properties (&rest forms)
   `(properties* ,@(mapcar (lambda (x) `(quote ,x)) forms)))
 
-
-(defun flatten (l)
-  (cond ((null l) nil)
-        ((atom l) (list l))
-        (t (loop for a in l appending (flatten a)))))
+(defun last-satisfies (pred coll)
+  (-> pred
+      (remove-if-not coll)
+      last
+      car))
 
 (defmacro functions (&rest forms)
   (declare (optimize (debug 3)))
@@ -151,214 +145,251 @@ type name (prin1-to-string default)))))
 						 param-list))))
 		       (format nil "~{~A~^, ~}"))))
       (format t "virtual ~A ~A(~A) = 0;~%" type name params))))
-
-(defun fill-property-names (classname)
-  (format t "~A() {~%" classname)
-       
-  (mapcar (lambda (property-pair i)
-	    (let ((name (car property-pair)))
-	      (format t "r[~D] = \"~A\";~%" i name)))
-	  property-container
-	  (range (length property-container)))
-
-  (format t "}~%")
-  
-  (format t "const char* type_name(const char* propertyname) {~%")
-
-  (->> property-container
-       (mapcar (lambda (x)
-		 (let ((name (car x))
-		       (type (regex-replace-all "__"
-					    (format nil "~a" (cdr x))
-					    "::")))
-		   (format t "if (strcmp(propertyname, \"~A\") == 0) return \"~A\";~%" name type)))))
-  (format t "return \"\";~%}~%")
-
-  (format t "const char** names() { return r; }~%")
-  (format t "int property_count = ~a;~%" (length property-container))
-
-  (format t "template<typename T>~% void set(const char* propertyname, T value) { ~%")
-  (->> property-container
-       (mapcar (lambda (x)
-		 (let ((name (car x)))
-		   (format t "if(strcmp(propertyname, \"~A\") == 0) ~A_field = value;~%" name name)))))
-  (format t "}~% template<typename T>~% T get(const char* propertyname, bool* success) {~%")
-  (->> property-container
-       (mapcar (lambda (x)
-		 (let ((name (car x)))
-		   (format t "if(strcmp(propertyname, \"~A\") == 0) {~%*success = true; ~% return ~A_field; ~%}~%" name name)))))
-  (format t "/////~%")
-  (format t "T t;~% *success = false; ~% return t; ~%}~%")
-  (format t "/////~%")
-  (format t "const char* r[~D];~%" (length property-container)))
-
-(defmacro inherits (&rest classes)
-  (format t "~{~a~^,~}~%{~%" (mapcar (lambda (class)
-				    (regex-replace-all "__"
-						       (format nil "~a" class)
-						       "::"))
-				    classes)))
-
-(defmacro defcppclass (class include-list &rest forms)
-  (let ((classname (cadr class))
-	(contains-inherit? (> (->> forms
-				(remove-if-not (lambda (f)
-						 (eq (car f) 'inherits)))
-				length) 0)))
-    (format t "#ifndef ~a~%#define ~a~%" classname classname)
-    (format t "//// generated at ~a~%" (now))
-    (format t "#include<cstring>~%")
-    (format t "#include<propertierbase.h>~%")
-    (dolist (file include-list)
-      (format t "#include~a~%" file))
-
-    (format t "class ~A: public propertierbase ~%" classname)
-
-    (if contains-inherit?
-	(format t ", ")
-	(format t " { ~%"))
     
-    (dolist (form forms)
-      (eval form))
-    (format t "public: ~%")
-    (fill-property-names classname)
-    (format t "};~%")
-    (setf *compiled-class* class)
-    (format t "#endif")))
-
-(defvar *baseclass-header*
-  "#ifndef propertierbase
-#define propertierbase
-//// generated at #<date-time 2017-06-17 18:44:10.914 {10067809C3}>
-
-class propertierbase
-{
-public:
-  propertierbase() {}
-  ~propertierbase() {}
-  virtual const char* type_name(const char* propertyname) = 0;
-  const char** names();
-  template<typename T>
-  virtual void set(const char* propertyname, T value);
-  
-  template<typename T>
-  virtual T get(const char* propertyname, bool* success);
-};
-#endif
-")
-
-(defun handle-base-class ()
-  (let ((header-filename (str *output-dir* "/propertierbase.h")))
-    (unless (probe-file header-filename)
-      (with-open-file (f header-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
-	(write-string *baseclass-header* f)))))
-
 (defun headers (forms)
   (let ((*print-case* :downcase))
-    (handle-base-class)
-    (setf *compiled-class* nil)
     (-> forms
 	eval)))
 
-
-(defun source (forms)
+(defun source (forms class-object)
   (declare (optimize (debug 3)))
   (let ((*print-case* :downcase)
-	(classname (cadadr forms)))
-    (format t "#include <~a.h>~%" classname)
+	(class-name (cadadr forms)))
+    (setf (classname class-object) class-name)
+    (format t "#include <~a.h>~%" class-name)
     (format t "///////// CPP FILE STARTS HERE, generated at ~a~%" (now))
-    (dolist (x property-container)
+    (dolist (x (propertylist class-object))
       (let ((name (car x))
 	    (type (regex-replace-all "__"
 				     (format nil "~a" (cdr x))
 				     "::")))
 	(format t "~A ~A::get~A() {~% return ~A_field; ~%};~%"
-		type classname (-> name symbol-name string-capitalize) name)
-	(format t "void ~A::set~A(~A val) {~% ~A_field = val; ~%}~%" classname (-> name symbol-name string-capitalize) type name)))))
+		type class-name (-> name symbol-name string-capitalize) name)
+	(format t "void ~A::set~A(~A val) {~% ~A_field = val; ~%}~%" class-name (-> name symbol-name string-capitalize) type name)))))
 
-(defvar running? t)
-(defvar *compilation-queue* (make-queue))
+(defun load-def-file (path)
+  (let* ((file-contents (read-file path))
+	 (headd (stdout-str (headers file-contents)))
+	 (obj (make-instance 'propertyclass))
+	 (cpp (stdout-str (source file-contents obj)))
+	 (header-filename (str *output-dir* "/" (format nil "~a" (classname obj)) ".h"))
+	 (cpp-filename (str *output-dir* "/" (format nil "~a" (classname obj)) ".cpp")))
+    (setf (src-str obj) cpp)
+    (setf (header-str obj) headd)
+    (setf (header-filename obj) header-filename)
+    (setf (src-filename obj) cpp-filename)
+    obj))
 
-(defun set-watch-on (dirpath)
-  (setf running? t)
-  (make-thread (lambda ()
-		 (in-package #:propertier)
-		 (open-fsnotify)
-		 (add-watch dirpath)
-		 (loop while running?
-		    do (let ((e (get-events)))
-			 (dolist (event e)
-			   (let ((path (car event))
-				 (type (cdr event)))
-			     (when (or (eq type :CREATE)
-				       (eq type :MODIFY))
-			       (push-queue path *compilation-queue*))))
-			 (sleep 3))))))
+    ;; (format t "Outputting sources to ~A and ~A~%" header-filename cpp-filename)
+    ;; 		(when (classname obj)
+    ;; 		  (with-open-file (f header-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
+    ;; 		    (write-string headd f))
+    ;; 		  (with-open-file (f cpp-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
+    ;; 		    (write-string cpp f))
+    ;; 		  (setf *compiled-class* nil)
+    ;; 		  (format t "Wrote files ~a and ~a~%" header-filename cpp-filename))))
+    ;; 	  (sleep 2))))
 
-(defmacro stdout-str (&rest forms)
-  (let ((s (gensym)))
-  `(let* ((,s (make-string-output-stream))
-	  (*standard-output* ,s))
-     (progn
-       ,@forms)
-     (get-output-stream-string ,s))))
+    
+  
+;; ;; (ql:quickload "cl-arrows")
+;; ;; (ql:quickload "quickproject")
+;; ;; (ql:quickload "cl-fsnotify")
+;; ;; (ql:quickload :lparallel)
+;; ;; (ql:quickload :simple-date-time)
+;; ;; (ql:quickload :cl-ppcre)
+;; ;; (require :quickproject)
 
-(defun compilation-server-loop ()
-  (loop while running?
-     do (let* ((*print-case* :downcase)
-	       (to-compile (pop-queue *compilation-queue*))
-	       (file-type (-> (format nil "~A" to-compile)					     
-			      reverse
-			      (subseq 0 3)
-			      reverse)))
-	  (setf property-container '())
-	  (if (and (string= file-type "def")
-		   (probe-file to-compile))
-	      (let* ((file-contents (read-file to-compile))
-		     (headd (stdout-str (headers file-contents)))
-		     (cpp (stdout-str (source file-contents)))
-		     (header-filename (str *output-dir* "/" (format nil "~a" (cadr *compiled-class*)) ".h"))
-		     (cpp-filename (str *output-dir* "/" (format nil "~a" (cadr *compiled-class*)) ".cpp")))
-		(format t "Outputting sources to ~A and ~A~%" header-filename cpp-filename)
-		(when *compiled-class*
-		  (with-open-file (f header-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
-		    (write-string headd f))
-		  (with-open-file (f cpp-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
-		    (write-string cpp f))
-		  (setf *compiled-class* nil)
-		  (format t "Wrote files ~a and ~a~%" header-filename cpp-filename))))
-	  (sleep 2))))
+;; (defun range (start &optional end)
+;;   (if end
+;;       (loop for i from start below end collect i)
+;;       (loop for i from 0 below start collect i)))
 
-(defvar *building?* nil)
-(defun start-compilation-server ()
-  (set-watch-on *input-dir*)
 
-  (if *building?*
-      (compilation-server-loop)
-      (make-thread #'compilation-server-loop)))
+;; (defun flatten (l)
+;;   (cond ((null l) nil)
+;;         ((atom l) (list l))
+;;         (t (loop for a in l appending (flatten a)))))
 
-(defun get-argv (key)
-  (let ((key (if (symbolp key)
-		 key
-		 (read-from-string key))))
-    (assert (= (mod (length (cdr sb-ext:*posix-argv*)) 2) 0))
-    (getf (plistify (cdr sb-ext:*posix-argv*)) key)))
 
-(defun main ()
-  (setf running? nil)
-  (unless *output-dir*
-    (setf *output-dir* (get-argv "--output-dir")))
-  (unless *input-dir*
-      (setf *input-dir* (get-argv "--input-dir")))
-  (mapcar (lambda (x)
-	    (pop-queue *compilation-queue*)) (range (queue-count *compilation-queue*)))
+;; (defun fill-property-names (classname)
+;;   (format t "~A() {~%" classname)
+       
+;;   (mapcar (lambda (property-pair i)
+;; 	    (let ((name (car property-pair)))
+;; 	      (format t "r[~D] = \"~A\";~%" i name)))
+;; 	  property-container
+;; 	  (range (length property-container)))
 
-  (cond
-    ((not *output-dir*) (format t "You need to set value to *output-dir*. Command line syntax is \"--output-dir path\". Call (main) again after setfing it"))
-    ((not *input-dir*) (format t "You need to set value to *input-dir*. Command line syntax is \"--input-dir path\". Call (main) again after setfing it"))
-    (t (start-compilation-server))))
+;;   (format t "}~%")
+  
+;;   (format t "const char* type_name(const char* propertyname) {~%")
 
-(defun build! ()
-  (setf *building?* t)
-  (sb-ext:save-lisp-and-die "./image" :toplevel #'main :executable t))
+;;   (->> property-container
+;;        (mapcar (lambda (x)
+;; 		 (let ((name (car x))
+;; 		       (type (regex-replace-all "__"
+;; 					    (format nil "~a" (cdr x))
+;; 					    "::")))
+;; 		   (format t "if (strcmp(propertyname, \"~A\") == 0) return \"~A\";~%" name type)))))
+;;   (format t "return \"\";~%}~%")
+
+;;   (format t "const char** names() { return r; }~%")
+;;   (format t "int property_count = ~a;~%" (length property-container))
+
+;;   (format t "template<typename T>~% void set(const char* propertyname, T value) { ~%")
+;;   (->> property-container
+;;        (mapcar (lambda (x)
+;; 		 (let ((name (car x)))
+;; 		   (format t "if(strcmp(propertyname, \"~A\") == 0) ~A_field = value;~%" name name)))))
+;;   (format t "}~% template<typename T>~% T get(const char* propertyname, bool* success) {~%")
+;;   (->> property-container
+;;        (mapcar (lambda (x)
+;; 		 (let ((name (car x)))
+;; 		   (format t "if(strcmp(propertyname, \"~A\") == 0) {~%*success = true; ~% return ~A_field; ~%}~%" name name)))))
+;;   (format t "/////~%")
+;;   (format t "T t;~% *success = false; ~% return t; ~%}~%")
+;;   (format t "/////~%")
+;;   (format t "const char* r[~D];~%" (length property-container)))
+
+;; (defmacro inherits (&rest classes)
+;;   (format t "~{~a~^,~}~%{~%" (mapcar (lambda (class)
+;; 				    (regex-replace-all "__"
+;; 						       (format nil "~a" class)
+;; 						       "::"))
+;; 				    classes)))
+
+;; (defmacro defcppclass (class include-list &rest forms)
+;;   (let ((classname (cadr class))
+;; 	(contains-inherit? (> (->> forms
+;; 				(remove-if-not (lambda (f)
+;; 						 (eq (car f) 'inherits)))
+;; 				length) 0)))
+;;     (format t "#ifndef ~a~%#define ~a~%" classname classname)
+;;     (format t "//// generated at ~a~%" (now))
+;;     (format t "#include<cstring>~%")
+;;     (format t "#include<propertierbase.h>~%")
+;;     (dolist (file include-list)
+;;       (format t "#include~a~%" file))
+
+;;     (format t "class ~A: public propertierbase ~%" classname)
+
+;;     (if contains-inherit?
+;; 	(format t ", ")
+;; 	(format t " { ~%"))
+    
+;;     (dolist (form forms)
+;;       (eval form))
+;;     (format t "public: ~%")
+;;     (fill-property-names classname)
+;;     (format t "};~%")
+;;     (setf *compiled-class* class)
+;;     (format t "#endif")))
+
+;; (defvar *baseclass-header*
+;;   "#ifndef propertierbase
+;; #define propertierbase
+;; //// generated at #<date-time 2017-06-17 18:44:10.914 {10067809C3}>
+
+;; class propertierbase
+;; {
+;; public:
+;;   propertierbase() {}
+;;   ~propertierbase() {}
+;;   virtual const char* type_name(const char* propertyname) = 0;
+;;   const char** names();
+;;   template<typename T>
+;;   virtual void set(const char* propertyname, T value);
+  
+;;   template<typename T>
+;;   virtual T get(const char* propertyname, bool* success);
+;; };
+;; #endif
+;; ")
+
+;; (defun handle-base-class ()
+;;   (let ((header-filename (str *output-dir* "/propertierbase.h")))
+;;     (unless (probe-file header-filename)
+;;       (with-open-file (f header-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
+;; 	(write-string *baseclass-header* f)))))
+
+
+
+;; (defvar running? t)
+;; (defvar *compilation-queue* (make-queue))
+
+;; (defun set-watch-on (dirpath)
+;;   (setf running? t)
+;;   (make-thread (lambda ()
+;; 		 (in-package #:propertier)
+;; 		 (open-fsnotify)
+;; 		 (add-watch dirpath)
+;; 		 (loop while running?
+;; 		    do (let ((e (get-events)))
+;; 			 (dolist (event e)
+;; 			   (let ((path (car event))
+;; 				 (type (cdr event)))
+;; 			     (when (or (eq type :CREATE)
+;; 				       (eq type :MODIFY))
+;; 			       (push-queue path *compilation-queue*))))
+;; 			 (sleep 3))))))
+
+;; (defun compilation-server-loop ()
+;;   (loop while running?
+;;      do (let* ((*print-case* :downcase)
+;; 	       (to-compile (pop-queue *compilation-queue*))
+;; 	       (file-type (-> (format nil "~A" to-compile)					     
+;; 			      reverse
+;; 			      (subseq 0 3)
+;; 			      reverse)))
+;; 	  (setf property-container '())
+;; 	  (if (and (string= file-type "def")
+;; 		   (probe-file to-compile))
+;; 	      (let* ((file-contents (read-file to-compile))
+;; 		     (headd (stdout-str (headers file-contents)))
+;; 		     (cpp (stdout-str (source file-contents)))
+;; 		     (header-filename (str *output-dir* "/" (format nil "~a" (cadr *compiled-class*)) ".h"))
+;; 		     (cpp-filename (str *output-dir* "/" (format nil "~a" (cadr *compiled-class*)) ".cpp")))
+;; 		(format t "Outputting sources to ~A and ~A~%" header-filename cpp-filename)
+;; 		(when *compiled-class*
+;; 		  (with-open-file (f header-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
+;; 		    (write-string headd f))
+;; 		  (with-open-file (f cpp-filename :direction :output :if-exists :overwrite :if-does-not-exist :create)
+;; 		    (write-string cpp f))
+;; 		  (setf *compiled-class* nil)
+;; 		  (format t "Wrote files ~a and ~a~%" header-filename cpp-filename))))
+;; 	  (sleep 2))))
+
+;; (defvar *building?* nil)
+;; (defun start-compilation-server ()
+;;   (set-watch-on *input-dir*)
+
+;;   (if *building?*
+;;       (compilation-server-loop)
+;;       (make-thread #'compilation-server-loop)))
+
+;; (defun get-argv (key)
+;;   (let ((key (if (symbolp key)
+;; 		 key
+;; 		 (read-from-string key))))
+;;     (assert (= (mod (length (cdr sb-ext:*posix-argv*)) 2) 0))
+;;     (getf (plistify (cdr sb-ext:*posix-argv*)) key)))
+
+;; (defun main ()
+;;   (setf running? nil)
+;;   (unless *output-dir*
+;;     (setf *output-dir* (get-argv "--output-dir")))
+;;   (unless *input-dir*
+;;       (setf *input-dir* (get-argv "--input-dir")))
+;;   (mapcar (lambda (x)
+;; 	    (pop-queue *compilation-queue*)) (range (queue-count *compilation-queue*)))
+
+;;   (cond
+;;     ((not *output-dir*) (format t "You need to set value to *output-dir*. Command line syntax is \"--output-dir path\". Call (main) again after setfing it"))
+;;     ((not *input-dir*) (format t "You need to set value to *input-dir*. Command line syntax is \"--input-dir path\". Call (main) again after setfing it"))
+;;     (t (start-compilation-server))))
+
+;; (defun build! ()
+;;   (setf *building?* t)
+;;   (sb-ext:save-lisp-and-die "./image" :toplevel #'main :executable t))
 
