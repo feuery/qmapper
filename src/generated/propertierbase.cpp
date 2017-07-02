b76130e80ef2264b56870b0166f2c39ba3391cda
Feuer <ilpo.lehtinen.1@saunalahti.fi>
2017-07-02 13:37:53 +0300

    Propertierbase::get(name, T val) and T Propertierbase::get(name) have empty default implementations now

--- src/generated/propertierbase.cpp
+++ src/generated/propertierbase.cpp
@@ -8,3 +8,14 @@ Propertierbase::~Propertierbase()
 {
 
 }
+
+void Propertierbase::set(const char* propertyname, unsigned long int value)
+ { }
+unsigned long int Propertierbase::get(const char* propertyname, bool *success, unsigned long int* type_helper) 
+{ *success = false; unsigned long int a; return a; }
+
+
+void Propertierbase::set(const char* propertyname, int value)
+ { }
+int Propertierbase::get(const char* propertyname, bool *success, int* type_helper) 
+{ *success = false; int a; return a; }
