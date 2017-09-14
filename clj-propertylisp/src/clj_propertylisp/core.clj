aee0791ada5ac6ad22f29a8509daa2243203c0cb
= <=>
2017-09-14 19:38:51 +0300

    We have a real tokenizer

--- clj-propertylisp/src/clj_propertylisp/core.clj
+++ clj-propertylisp/src/clj_propertylisp/core.clj
@@ -2,15 +2,12 @@
 ;; Mostly because I don't dislike cider-repl as much as I dislike what SLIME does
 (ns clj-propertylisp.core
   (:require [hawk.core :as hawk]
+            [clojure.core.async :refer [go go-loop <! >! chan]]
             [clojure.java.io :as io]))
 
 (def input-dir (atom nil))
 (def output-dir (atom nil))
-(def compilation-queue (atom []))
-
-(add-watch compilation-queue :lol (fn [_ _ _ a]
-                                      (println "Added stuff to compilation queue")
-                                      (clojure.pprint/pprint a)))
+(def compilation-queue (chan))
 
 (defn getContainerDirPath [path]
   (io/file (.getParent (if (string? path)
@@ -18,7 +15,7 @@
                          path))))
 
 (defn push-compilation! [content]
-  (swap! compilation-queue conj content))
+  (>! compilation-queue content))
 
 (defn handler [ctx {:keys [kind file]}]                             
   (when (and (not= kind :delete)
@@ -54,5 +51,62 @@
 ;; and push THAT to the *compilation-queue*
 
 
-(start-compilation-d! "/home/feuer2/Dropbox/qt-test/src/lisp-defs"
+#_(start-compilation-d! "/home/feuer2/Dropbox/qt-test/src/lisp-defs"
                       "/home/feuer2/Dropbox/qt-test/src/generated")
+
+(def test-data '(defcppclass map ("<root.h>" "<layer.h>" "<vector>" "<string>")
+                  (declare-class "root" "layer")
+                  (public
+                   (fields
+                    (std__vector<Layer*>* layers nullptr))
+                   (properties
+                    (std__string name "Map 1"))
+                   (functions
+                    (void parent (root* p)))
+                   (functions
+                    (int width (void ))
+                    (int height (void))
+                    (root* parent (void))))
+                  
+                  (private
+                   (fields
+                    (std__vector<Layer*>* layers nullptr))
+                   (properties
+                    (std__string name "Map 1"))
+                   (functions
+                    (void parent (root* p)))
+                   (functions
+                    (int width (void ))
+                    (int height (void))
+                    (root* parent (void))))))
+
+(defn tokenize-type [privacy-level [f & forms]]
+  (map (fn [form]
+         {:form form
+          :privacy privacy-level
+          :type f})
+       forms))
+
+(defn tokenize-privacy-level [[f & forms]]
+  (map (partial tokenize-type f) forms))
+  
+
+(defn tokenize [def]
+  (->> def
+       (filter seq?)
+       (filter (fn [[s & lol]]
+                 (contains? #{'public 'private 'protected} s)))
+
+       (mapcat tokenize-privacy-level)))
+
+
+(def get-functions (partial #'get-symbol-group 'functions))
+(def get-properties (partial #'get-symbol-group 'properties))
+(def get-fields (partial #'get-symbol-group 'fields))
+               
+
+(defn start-compiler-backend! []
+  (go-loop [compilation-set (<! compilation-queue)]
+    (doseq [[file-name content] compilation-set
+            :let [content-as-lisp (read-line content)
+                  
